f1e9e18bfb3772e8284408591aecaacff6866870
Ben Papillon <benpapillon@gmail.com>
2020-12-06 20:35:05 -0500

    day 1 cleanup

--- 01/main.py
+++ 01/main.py
@@ -9,12 +9,12 @@ def combo_sum(combo):
     return reduce(lambda x, y: x + y, combo)
 
 
-def product_of_entries(combination_size):
-    integers = map(int, open('input.txt', 'rb').read().strip().split("\n"))
-    combinations = itertools.combinations(integers, combination_size)
+def product_of_entry_combination(entries, combination_size):
+    combinations = itertools.combinations(entries, combination_size)
     combination = [combination for combination in combinations if combo_sum(combination) == 2020][0]
     return combo_product(combination)
 
 
-print 'part 1: ' + str(product_of_entries(2))
-print 'part 2: ' + str(product_of_entries(3))
+integers = map(int, open('input.txt', 'rb').read().strip().split("\n"))
+print 'part 1: ' + str(product_of_entry_combination(integers, 2))
+print 'part 2: ' + str(product_of_entry_combination(integers, 3))
